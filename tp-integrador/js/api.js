2e7375cb2517afe01a075c2acc95053b10e7c43e
azamma <azamma@users.noreply.github.com>
2019-04-19 01:21:59 -0300

    agregado los filtros en pantalla

--- tp-integrador/js/api.js
+++ tp-integrador/js/api.js
@@ -1,11 +1,21 @@
 $(function() {
 	
 	
-    	$.ajax({
+	
+	function buscar(param) {
+		
+	//Limpio los divs
+	for (var i = 0; i < 10; i++) {
+	$( "#produ"+i ).empty();
+	}
+	
+	var limite=40;
+		
+	$.ajax({
 	//El tipo de petición
 	type: 'GET',
 	//La url de la api. Al final de la dirección se le pasa el término de la búsqueda
-	url: 'https://api.mercadolibre.com/sites/MLA/search?q=memoriaram',
+	url: 'https://api.mercadolibre.com/sites/MLA/search?q='+param+'&category=MLA1648&shipping=free&sort=price_desc&offset='+limite,
 	
 	//Si la api da una respuesta de éxito agarra la información que da y la llamo resultados
 	success: function(resultados){
@@ -13,7 +23,7 @@ $(function() {
 		//para cada resultado de los elementos results
 		$.each(resultados.results, function(i,resultado){	
 		
-			if(i<11){
+			if(i<limite+1){
 				
 			$("#produ"+i).append('<p id="nombre"> Nombre: </p><p>' + resultado.title +' </p>');
 			$("#produ"+i).append('<p id="precio"> Precio: </p>$<p>' + resultado.price +' </p>');
@@ -24,4 +34,11 @@ $(function() {
 		})
 	}
 	});
+		
+	}
+	
+		
+	$('#buscador').click(function() {  
+	 buscar($('#campo').val());
+    }); 
 });
