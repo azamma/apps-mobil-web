2048f5bc19f05bb3e40375b50bef81fef1f5b0dc
azamma <azamma@users.noreply.github.com>
2019-04-20 22:06:47 -0300

    algunas modificaciones

--- tp-integrador/js/api.js
+++ tp-integrador/js/api.js
@@ -9,13 +9,13 @@ $(function() {
 	$( "#produ"+i ).empty();
 	}
 	
-	var limite=40;
+	var limite=10;
 		
 	$.ajax({
 	//El tipo de petición
 	type: 'GET',
 	//La url de la api. Al final de la dirección se le pasa el término de la búsqueda
-	url: 'https://api.mercadolibre.com/sites/MLA/search?q='+param+'&category=MLA1648&shipping=free&sort=price_desc&offset='+limite,
+	url: 'https://api.mercadolibre.com/sites/MLA/search?q=memoria ram'+param+'&category=MLA1648&shipping=free&sort=price_desc&offset='+limite,
 	
 	//Si la api da una respuesta de éxito agarra la información que da y la llamo resultados
 	success: function(resultados){
@@ -25,9 +25,10 @@ $(function() {
 		
 			if(i<limite+1){
 				
-			$("#produ"+i).append('<p id="nombre"> Nombre: </p><p>' + resultado.title +' </p>');
-			$("#produ"+i).append('<p id="precio"> Precio: </p>$<p>' + resultado.price +' </p>');
+			$("#produ"+i).append('<p> Nombre: </p><p id="nombre'+i+'">' + resultado.title +' </p>');
+			$("#produ"+i).append('<p> Precio: </p>$<p id="precio'+i+'">' + resultado.price +' </p>');
 			$("#produ"+i).append('<img src=' + resultado.thumbnail +'>');
+			$("#produ"+i).append('<div class="button"><input id="share'+i+'" type="button" onclick="share(this.id);" value="Compartir"/></div>');
 			
 			
 			} 
@@ -41,4 +42,22 @@ $(function() {
 	$('#buscador').click(function() {  
 	 buscar($('#campo').val());
     }); 
+	
+	
+	
 });
+
+function share(id){
+	var produnumero = id.substr(id.length-1) ;
+	alert( 'Se va a compartir'+$('#nombre'+produnumero).text() );
+
+}
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
